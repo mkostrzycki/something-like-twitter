db91c3c08b6e453e40f997462baa2cc8d1d1fe07
Maciej Kostrzycki <maciej.kostrzycki@gmail.com>
2017-03-03 22:52:26 +0100

    Added jumbotron. Changed navigation bar.

--- index.php
+++ index.php
@@ -22,26 +22,51 @@ $tweets = Tweet::loadAllTweets($conn);
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Something Like Tweeter - Main Page</title>
+        <title>Something Like Twitter - Main Page</title>
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" media="screen" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <link rel="stylesheet" href="css/style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <body>
-        <div class="container">
-            <div class="row nav">
-                <nav>
-                    <ul>
-                        <li><a href="#"><b>Home</b></a></li>
-                        <li><a href="#">Page 1</a></li>
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>                        
+                    </button>
+                    <a class="navbar-brand" href="#">Something Like Twitter</a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Page 1-1</a></li>
+                                <li><a href="#">Page 1-2</a></li>
+                                <li><a href="#">Page 1-3</a></li>
+                            </ul>
+                        </li>
                         <li><a href="#">Page 2</a></li>
+                        <li><a href="#">Page 3</a></li>
                     </ul>
-                    <div class="login-btn">
-                        <a href="page_register.php">Sign Up</a>
-                        <a href="page_login.php">Sign In</a>
-                    </div>
-                </nav>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="page_register.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                        <li><a href="page_login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    </ul>
+                </div>
             </div>
+        </nav>
+        <div class="jumbotron text-center">
+            <h1>Welcome, dear Guest <span class="glyphicon glyphicon-sunglasses"></span></h1>
+            <p>This page is a simple clone of twitter.
+                Feel free and tweet something :)</p>
+        </div>
+        <div class="container">
             <div class="row tweet-send">
                 <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                     <div class="form-group">
@@ -53,16 +78,14 @@ $tweets = Tweet::loadAllTweets($conn);
                 </div>
             </div>
             <div class="row">
-                
-                    <?php
-                    foreach ($tweets as $tweet) {
-                        echo '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 tweet">';
-                        echo '<p>' . $tweet->getText() . '</p>';
-                        echo '<span class="tweet-date">' . $tweet->getCreationDate() . '</span>';
-                        echo '</div>';
-                    }
-                    ?>
-               
+                <?php
+                foreach ($tweets as $tweet) {
+                    echo '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 tweet">';
+                    echo '<p>' . $tweet->getText() . '</p>';
+                    echo '<span class="tweet-date">' . $tweet->getCreationDate() . '</span>';
+                    echo '</div>';
+                }
+                ?>
             </div>
         </div>
     </body>
